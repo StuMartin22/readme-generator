6991b05f3343e62d4c4e6bb3c59c826bd95a29b9
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-14 00:04:05 -0500

    continued changes to proposed styling for readme. Changed file location and name.

--- assets/utils/readmestyling.js
+++ assets/utils/readmestyling.js
@@ -1,18 +1,6 @@
-// WHAT I NEED
-// Title, 
-//technologies? dropdown, HTML, CSS, JAVASCRIPT, NODEJS,
-// description ABOUT -- IMAGES
-// table of contents (Autofill with top, installation, usage, contributing, tests, licensing, questions)
-// installation,
-// usage,
-// contributing,
-// tests,
-// license (&badge) - list of options,
-// questions -- email section w/ instructions how to reach me, 
-// links take user to corresponding section of readme
-
 const generateMarkdown = ({ title, html, css, javascript, about, screenCap, screenCapTwo, screenCapThree, productionVideo, functionality, installation, contributions, liveLink, repoLink, authorName, email, authorGitHub  }) =>
-<h1 align="center"> ${title} </h1>
+
+`<h1 align="center"> ${title} </h1>
 <h1 align="center"></h1>
 
 ---
@@ -24,15 +12,15 @@ ${about}
 ---
 
 <h1 align="center">Table of Contents</h1>
-<h2 alight="center">About</h2>
-<h2 alight="center">Technologies Used</h2>
-<h2 alight="center">Images and Videos</h2>
-<h2 alight="center">Installation</h2>
-<h2 alight="center">Usage</h2>
-<h2 alight="center">Contributing</h2>
-<h2 alight="center">Tests</h2>
-<h2 alight="center">License</h2>
-<h2 alight="center">Questions?</h2>
+<h2 align="center">About</h2>
+<h2 align="center">Technologies Used</h2>
+<h2 align="center">Images and Videos</h2>
+<h2 align="center">Installation</h2>
+<h2 align="center">Usage</h2>
+<h2 align="center">Contributing</h2>
+<h2 align="center">Tests</h2>
+<h2 align="center">License</h2>
+<h2 align="center">Questions?</h2>
 
 ---
 
@@ -54,7 +42,7 @@ ${productionVideo}
 
 ---
 
-<h1 alight="center">Installation</h1>
+<h1 align="center">Installation</h1>
 
 ${installation}
 
@@ -85,9 +73,5 @@ ${contributions}
 <h1 align="center">Questions? Contact Me!</h1>
 
 <p align="center">${authorName}</p>
-<p alight="center">${email}</p>
-<p align="center"><a href="https://github.com/" + ${authorGitHub} >GitHub Link</a></p>
-
-
-
-
+<p align="center">${email}</p>
+<p align="center"><a href="https://github.com/${authorGitHub}" >GitHub Link</a></p>`
