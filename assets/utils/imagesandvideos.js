455d7940ff45ca8aefa8f537362c4c34aed07168
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-04-14 21:06:21 -0500

    added logic for images and video questions and insertions.

---
+++ assets/utils/imagesandvideos.js
@@ -0,0 +1,21 @@
+function renderImages (answers){
+    if(answers.images === 'one'){
+        return `![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")`
+    }else if (answers.images === 'two'){
+        return `![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")``![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")`
+    }else if (answers.images === 'three'){
+        return `![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")``![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")``![Image of WHATEVER THE IMAGE IS GOES HERE. IMAGE ALT TEXT GOES HERE.]( ./LOCALFILEPATHHERE "NAMEOFIMAGEHERE")`
+    }else{
+        return ``
+    }
+};
+
+function renderVideo (answers){
+    if (answers.productionVideo === 'Y'){
+        return `<a href="VIDEOURLGOESHERE">TEXTFORVIDEOLINKGOESHERE</a>`
+    }else if (answers.productionVideo === 'N'){
+        return ``
+    }else {
+        return ``
+    }
+};
